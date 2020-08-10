e7c136de98753f85697e972fa095535347fb3d71
AlsonZ <48876218+AlsonZ@users.noreply.github.com>
2020-08-11 02:25:21 +1000

    fix navbar background and sidebar z-index

--- index.js
+++ index.js
@@ -2,3 +2,14 @@ let menuBtn = document.getElementById('menuButton');
 menuBtn.addEventListener('click', () => {
   document.getElementById('sidebar').classList.toggle('sidebar-active')
 });
+
+let navbar = document.getElementById('navbar')
+
+window.addEventListener('scroll', (e) => {
+
+  if(window.scrollY > 10) {
+    navbar.classList.add('nav-collapse');
+  } else {
+    navbar.classList.remove('nav-collapse');
+  }
+});
